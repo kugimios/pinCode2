c2cd6a2e02fb462a857149a0c1337a03027f6969
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2018-01-13 10:48:34 +0300

    hobby added

--- pinCodeApp2/SecondViewController.swift
+++ pinCodeApp2/SecondViewController.swift
@@ -12,6 +12,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    var hobby:Array<String> = []
+    var phobia:Array<String> = []
+    
+    @IBOutlet weak var button: UIButton!
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
     }
     @IBOutlet weak var progressBarOutlet: UIProgressView!
@@ -27,9 +31,57 @@ class SecondViewController: UIViewController {
         print(progressBarOutlet.progress)
         segmentControllerOutlet.selectedSegmentIndex = 1
         
+        if(addTextFieldOutlet.text! != ""){
+            addoption(addTextFieldOutlet.text!)
+            addTextFieldOutlet.text = ""
+            labelcontrol()
+        }
+        
     }
     
+    func addoption(_ text:String)  {
+        
+        if(segmentControllerOutlet.selectedSegmentIndex == 0){
+            
+            hobby.append(text)
+            if(hobby.count == 5){
+                
+                segmentControllerOutlet.setEnabled(true, forSegmentAt: 1)
+            }
+        }else if(segmentControllerOutlet.selectedSegmentIndex == 1){
+            
+            phobia.append(text)
+            if(phobia.count == 5){
+                button.isEnabled = false
+            }
+        }
+    }
     
+    func labelcontrol()  {
+        showHobbyOrPhobiaOutlet.text = ""
+        progressBarOutlet.progress = 0
+        if(segmentControllerOutlet.selectedSegmentIndex == 0){
+            
+            for i in 0 ..< hobby.count{
+                progressBarOutlet.progress = Float(0.20 * Double(i+1))
+                addTextFieldOutlet.text = showHobbyOrPhobiaOutlet.text! + String(hobby[i]) + "\n"
+            }
+        }
+        else if segmentControllerOutlet.selectedSegmentIndex == 1{
+            for i in 0 ..< phobia.count{
+                
+                progressBarOutlet.progress = Float(0.20 * Double(i+1))
+                print(Float(0.20 * Double(i+1)))
+                showHobbyOrPhobiaOutlet.text = showHobbyOrPhobiaOutlet.text! + phobia[i] + "\n"
+            }
+        }
+    }
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        
+        print(segmentControllerOutlet.selectedSegmentIndex)
+        labelcontrol()
+    }
     
     
     @IBOutlet weak var showHobbyOrPhobiaOutlet: UILabel!
@@ -38,7 +90,10 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print(segmentControllerOutlet.selectedSegmentIndex)
+        print(progressBarOutlet.progress)
+        
+        segmentControllerOutlet.setEnabled(false, forSegmentAt: 1)
     }
 
     override func didReceiveMemoryWarning() {
